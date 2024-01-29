c187d76fabecdb3f8be36e338988e9373cc9aeb0
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-01-29 11:11:43 -0500

    `black` on examples

--- examples/demo.py
+++ examples/demo.py
@@ -8,41 +8,41 @@ from ezmsg.util.debuglog import DebugLog
 if __name__ == '__main__':
     import argparse
 
-    parser = argparse.ArgumentParser(description = "Consume data from NSP")
+    parser = argparse.ArgumentParser(description="Consume data from NSP")
 
     parser.add_argument(
         "--inst_addr",
         "-i",
-        type = str,
-        default = "192.168.137.128",
-        help = "ipv4 address of device. pycbsdk will send control packets to this address. Subnet OK. "
-        "Use 127.0.0.1 for use with nPlayServer (non-bcast). "
-        "The default is 0.0.0.0 (IPADDR_ANY) on Mac and Linux. On Windows, known IPs will be searched.",
+        type=str,
+        default="192.168.137.128",
+        help="ipv4 address of device. pycbsdk will send control packets to this address. Subnet OK. "
+             "Use 127.0.0.1 for use with nPlayServer (non-bcast). "
+             "The default is 0.0.0.0 (IPADDR_ANY) on Mac and Linux. On Windows, known IPs will be searched.",
     )
 
     parser.add_argument(
         "--inst_port",
-        type = int,
-        default = 51001,
-        help = "Network port to send control packets."
-        "Use 51002 for Gemini and 51001 for Legacy NSP.",
+        type=int,
+        default=51001,
+        help="Network port to send control packets."
+             "Use 51002 for Gemini and 51001 for Legacy NSP.",
     )
 
     parser.add_argument(
         "--client_addr",
         "-c",
-        type = str,
-        default = "",
-        help = "ipv4 address of this machine's network adapter we will receive packets on. "
-        "Defaults to INADDR_ANY. If address is provided, assumes Cerebus Subnet.",
+        type=str,
+        default="",
+        help="ipv4 address of this machine's network adapter we will receive packets on. "
+             "Defaults to INADDR_ANY. If address is provided, assumes Cerebus Subnet.",
     )
 
     parser.add_argument(
         "--client_port",
         "-p",
-        type = int,
-        default = 51002,
-        help = "Network port to receive packets. This should always be 51002.",
+        type=int,
+        default=51002,
+        help="Network port to receive packets. This should always be 51002.",
     )
 
     parser.add_argument(
@@ -50,23 +50,23 @@ if __name__ == '__main__':
         "-b",
         type=int,
         help=f"UDP socket recv buffer size. "
-        f"Default: {(8 if sys.platform == 'win32' else 6) * 1024 * 1024}.",
+             f"Default: {(8 if sys.platform == 'win32' else 6) * 1024 * 1024}.",
     )
 
     parser.add_argument(
         "--protocol",
-        type = str,
-        default = "3.11",
-        help = "Protocol Version. 3.11, 4.0, or 4.1 supported.",
+        type=str,
+        default="3.11",
+        help="Protocol Version. 3.11, 4.0, or 4.1 supported.",
     )
 
     source = NSPSource(NSPSourceSettings(**vars(parser.parse_args())))
     log = DebugLog()
 
     ez.run(
-        SOURCE = source,
-        LOG = log,
-        connections = (
+        SOURCE=source,
+        LOG=log,
+        connections=(
             (source.OUTPUT_SPIKE, log.INPUT),
         )
     )

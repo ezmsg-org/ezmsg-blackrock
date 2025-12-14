199940e329982cfefdd3695aa9dc99f227ce8ef0
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-12-14 00:59:36 -0500

    ruff

--- examples/enable_cont.py
+++ examples/enable_cont.py
@@ -1,11 +1,10 @@
 import sys
 import time
-from typing_extensions import Annotated
 
+import typer
 from pycbsdk import cbsdk
 from pycbsdk.cbhw.packet.common import CBChannelType
-import typer
-
+from typing_extensions import Annotated
 
 smp_filters_for_grp = {
     1: 5,  # samp 500 Hz, filter LP 125 Hz
@@ -28,9 +27,7 @@ def main(
     ] = "192.168.137.128",
     inst_port: Annotated[
         int,
-        typer.Option(
-            help="Network port to send control packets. Use 51002 for Gemini and 51001 for Legacy NSP."
-        ),
+        typer.Option(help="Network port to send control packets. Use 51002 for Gemini and 51001 for Legacy NSP."),
     ] = 51001,
     client_addr: Annotated[
         str,
@@ -41,18 +38,14 @@ def main(
     ] = "",
     client_port: Annotated[
         int,
-        typer.Option(
-            help="Network port to receive packets. This should always be 51002."
-        ),
+        typer.Option(help="Network port to receive packets. This should always be 51002."),
     ] = 51002,
     recv_bufsize: Annotated[int, typer.Option(help="UDP socket recv buffer size.")] = (
         8 if sys.platform == "win32" else 6
     )
     * 1024
     * 1024,
-    protocol: Annotated[
-        str, typer.Option(help="Protocol Version. 3.11, 4.0, or 4.1 supported.")
-    ] = "3.11",
+    protocol: Annotated[str, typer.Option(help="Protocol Version. 3.11, 4.0, or 4.1 supported.")] = "3.11",
 ):
     params = cbsdk.create_params(
         inst_addr=inst_addr,
@@ -74,8 +67,7 @@ def main(
     for chid in [
         k
         for k, v in config["channel_infos"].items()
-        if config["channel_types"][k]
-        in (CBChannelType.FrontEnd, CBChannelType.AnalogIn)
+        if config["channel_types"][k] in (CBChannelType.FrontEnd, CBChannelType.AnalogIn)
     ]:
         if smp_group in smp_filters_for_grp:
             smp_filter = smp_filters_for_grp[smp_group]
